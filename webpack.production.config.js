6d701d0d4182b562d01805a9b2336e455c7077c7
imhzs <30996509+imhzs@users.noreply.github.com>
2017-09-29 14:31:16 +0800

    Add files via upload

---
+++ webpack.production.config.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    'react-hot-loader/patch',
+    'babel-polyfill',
+    path.join(__dirname, 'app/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    chunkFilename: '[name]-[hash:12].js',
+    filename: '[name]-[hash:12].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+      inject: 'body',
+      filename: './index.html'
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['.less', '.js', '.jsx', '.json', '.css'],
+    alias: {
+      components: path.resolve(__dirname, 'app/components/'),
+      containers: path.resolve(__dirname, 'app/containers'),
+      reducers: path.resolve(__dirname, 'app/redux/reducers'),
+      actions: path.resolve(__dirname, 'app/redux/actions'),
+      route: path.resolve(__dirname, 'app/route/'),
+      tools: path.resolve(__dirname, 'app/tools/')
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'app'),
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|woff2)(\?.+)?$/,
+        loader: 'file-loader?name=[hash:12].[ext]'
+      }
+    ]
+  }
+};
